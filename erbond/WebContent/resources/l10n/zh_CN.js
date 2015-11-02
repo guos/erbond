4f412d36bcd0b701897fd90fec31a77850d74475
Administrator <Administrator@SC-201505091144>
2015-11-02 08:47:42 +0800

    init

---
+++ erbond/WebContent/resources/l10n/zh_CN.js
@@ -0,0 +1,72 @@
+{
+
+"header" : {
+  "navbar" : {
+    "UPLOAD" : "上传",
+    "new" : {
+      "NEW" : "新建",
+      "PROJECT" : "项目",
+      "TASK" : "任务",
+      "USER" : "用户",
+      "EMAIL" : "Email"
+    },
+    "NOTIFICATIONS" : "通知"
+  }
+},
+"aside" : {
+  "nav" : {
+    "HEADER" : "导航",
+    "DASHBOARD" : "主页",
+    "CALENDAR" : "日历",
+    "EMAIL" : "Email",
+    "WIDGETS" : "插件",
+    "components" : {
+      "COMPONENTS" : "小工具",
+      "ui_kits" : {
+        "UI_KITS" : "UI Kits",
+        "BUTTONS" : "按钮",
+        "ICONS" : "图标",
+        "GRID" : "网格",
+        "BOOTSTRAP" : "Bootstrap",
+        "SORTABLE" : "Sortable",
+        "PORTLET" : "Portlet",
+        "TIMELINE" : "Timeline",
+        "VECTOR_MAP" : "Vector Map"
+      },
+      "table" : {
+        "TABLE" : "表格",
+        "TABLE_STATIC" : "表格统计",
+        "DATATABLE" : "数据表",
+        "FOOTABLE" : "Footable"
+      },
+      "form" : {
+        "FORM" : "表单",
+        "FORM_ELEMENTS" : "表单元素",
+        "FORM_VALIDATION" : "表单验证",
+        "FORM_WIZARD" : "Form wizard"
+      },
+      "CHART" : "图表",
+      "pages" : {
+        "PAGES" : "页面",
+        "PROFILE" : "个人资料",
+        "POST" : "发表",
+        "SEARCH" : "搜索",
+        "INVOICE" : "发货单",
+        "LOCK_SCREEN" : "锁屏",
+        "SIGNIN" : "登陆",
+        "SIGNUP" : "注册",
+        "FORGOT_PASSWORD" : "忘记密码",
+        "404" : "404"
+      }
+    },
+    "your_stuff" : {
+      "YOUR_STUFF": "Your Stuff",
+      "PROFILE" : "资料",
+      "DOCUMENTS" : "文档"
+    }
+  },
+  "MILESTONE" : "Milestone",
+  "RELEASE" : "Release"
+}
+
+}
